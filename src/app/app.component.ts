4d0ce14c64d5a3c59069bf2ce19120b6c303a9ee
Jure Jures <jure.04jj@gmail.com>
2024-11-27 13:04:53 +0100

    Inputs and outputs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  standalone: false,
 })
 export class AppComponent {
+  // three variables that are available
+  // to the template
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
 
-
-
+  onCourseSelected(course: Course): void {
+    console.log("app component - button clicked...", course);
+  }
 }
--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Course } from "src/app/model/course";
 
 @Component({
   selector: "course-card", // html tag of the app
@@ -7,5 +8,22 @@ import { Component, OnInit } from "@angular/core";
   styleUrl: "./course-card.component.css",
 })
 export class CourseCardComponent implements OnInit {
+  @Input({
+    required: true,
+  })
+  course: Course;
+
+  // Event emitter with the Course type
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
   ngOnInit(): void {}
+
+  onCourseViewed(course: Course): void {
+    console.log("card component - button clicked");
+
+    //emits a custom event
+    // emits -> odda -> sends a custom event
+    this.courseSelected.emit(this.course);
+  }
 }
