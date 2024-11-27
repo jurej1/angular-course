1ea43b2e9a62f9a11f5d881b8e71102de7b0f079
Jure Jures <jure.04jj@gmail.com>
2024-11-27 12:37:14 +0100

    1-lesson.

---
+++ src/app/components/course-card/course-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "course-card", // html tag of the app
+  imports: [],
+  templateUrl: "./course-card.component.html", // template file
+  styleUrl: "./course-card.component.css",
+})
+export class CourseCardComponent implements OnInit {
+  ngOnInit(): void {}
+}
