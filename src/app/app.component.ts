007ab7c882fb2afa73b8450e95098e91983857d2
Jure Jures <jure.04jj@gmail.com>
2024-11-27 17:05:17 +0100

    Components VIEW layer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,24 @@ import { Course } from "./model/course";
   standalone: false,
 })
 export class AppComponent {
-  // three variables that are available
-  // to the template
-  coreCourse = COURSES[0];
-  rxjsCourse = COURSES[1];
-  ngrxCourse = COURSES[2];
+  courses = [...COURSES];
+
+  title = COURSES[0].description;
+
+  startDate = new Date(2000, 0, 1);
+
+  price = 9.99;
+
+  rate = 0.67;
 
   onCourseSelected(course: Course): void {
     console.log("app component - button clicked...", course);
+  }
+
+  // tracking function
+  // index & element of the array
+  // function should return unique object
+  trackCourse(index: number, course: Course) {
+    return course.id;
   }
 }
--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from "@angular/common";
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { Course } from "src/app/model/course";
 
 @Component({
   selector: "course-card", // html tag of the app
-  imports: [],
+  imports: [CommonModule],
   templateUrl: "./course-card.component.html", // template file
   styleUrl: "./course-card.component.css",
 })
@@ -17,13 +18,26 @@ export class CourseCardComponent implements OnInit {
   @Output()
   courseSelected = new EventEmitter<Course>();
 
+  @Input({ required: true })
+  index: number;
+
   ngOnInit(): void {}
 
-  onCourseViewed(course: Course): void {
+  onCourseViewed(): void {
     console.log("card component - button clicked");
 
     //emits a custom event
     // emits -> odda -> sends a custom event
     this.courseSelected.emit(this.course);
+  }
+
+  cardClasses() {
+    if (this.course.category == "BEGINNER") {
+      return "beginner";
+    }
+  }
+
+  cardStyles() {
+    return { "background-image": "url(" + this.course.iconUrl + ")" };
   }
 }
